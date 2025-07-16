1eeab38e7f21e594c4f3347633e63f4a980f1ae9
Mcadefarhan <farhankhan22eu@gmail.com>
2025-07-16 14:21:33 +0530

    Add files via upload

---
+++ swapExample.java
@@ -0,0 +1,16 @@
+public class swapExample{
+    public static void main(String[] args) {
+        int a=4;
+        int b=6;
+        int c=8;
+
+        int temp =a;
+        a=b;
+        b=c;
+        c= temp;
+        
+
+        System.out.println("a="+a+"\nb="+b+"\nc="+c);
+        System.out.println(" ");
+    }
+}
